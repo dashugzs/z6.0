97985578b706031aef83b606beedfd2f474e0aef
dashugzs <87010009+dashugzs@users.noreply.github.com>
2025-09-16 11:15:54 +0800

    Update settings.js

--- js/settings.js
+++ js/settings.js
@@ -137,6 +137,7 @@ function createSettingsElements() {
         <div class="settings-section">
             <h3>用户中心</h3>
             <a href="admin.html" id="login-btn" target="_blank">管理员登录</a>
+            <a href="https://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=niu@xnss.fun" id="login-btn" target="_blank">联系邮箱:niu@xnss.fun</a>
         </div>
     `;
     document.body.appendChild(settingsPanel);
@@ -494,3 +495,4 @@ function updateSearchStyle() {
         searchInput.style.backgroundColor = isDarkMode ? 'var(--search-input-dark)' : 'var(--search-input-light)';
     }
 }
+
