12dbab26e6027f8fddf4d9ed54fca9ab045ec92e
dashugzs <87010009+dashugzs@users.noreply.github.com>
2025-10-13 16:24:55 +0800

    Update 1settings.js

--- js/shezhimb/1settings.js
+++ js/shezhimb/1settings.js
@@ -97,9 +97,9 @@ function loadOtherSettings() {
     
     // 渲染自定义壁纸
     renderCustomWallpapers();
-let darkMode = getSetting('darkMode');
-if (!darkMode) darkMode = 'auto';
-updateDarkModeButtons(darkMode);
+    let darkMode = getSetting('darkMode');
+    if (!darkMode) darkMode = 'auto';
+    updateDarkModeButtons(darkMode);
 }
 
 // 创建设置相关的DOM元素
@@ -535,5 +535,6 @@ function updateSearchStyle() {
         searchInput.style.backgroundColor = isDarkMode ? 'var(--search-input-dark)' : 'var(--search-input-light)';
     }
 }
+
 
 
