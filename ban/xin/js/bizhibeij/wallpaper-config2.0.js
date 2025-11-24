4f89b07d17b1aa40102440487bbb7b9460d3ffd4
dashugzs <87010009+dashugzs@users.noreply.github.com>
2025-11-24 19:48:09 +0800

    Add files via upload

---
+++ ban/xin/js/bizhibeij/wallpaper-config2.0.js
@@ -0,0 +1,37 @@
+// 壁纸配置文件，方便后期管理
+const wallpaperConfig = {
+  // 默认壁纸配置（6张）
+  defaultWallpapers: [
+    {
+      id: 'default1',
+      // 自动生成缩略图（使用原图链接，浏览器会自动缩放）
+      thumbUrl: 'https://img.alicdn.com/imgextra/i4/2327995847/O1CN017B14DT1t3ylJvwyge_!!2327995847.jpg',
+      fullUrl: 'https://img.alicdn.com/imgextra/i4/2327995847/O1CN017B14DT1t3ylJvwyge_!!2327995847.jpg'
+    },
+    {
+      id: 'default2',
+      thumbUrl: 'https://img.alicdn.com/imgextra/i2/2327995847/O1CN01RMpX341t3ylJvwqMr_!!2327995847.jpg',
+      fullUrl: 'https://img.alicdn.com/imgextra/i2/2327995847/O1CN01RMpX341t3ylJvwqMr_!!2327995847.jpg'
+    },
+    {
+      id: 'default3',
+      thumbUrl: 'https://img.alicdn.com/imgextra/i2/2327995847/O1CN01SJyRxT1t3ylGVThru_!!2327995847.jpg',
+      fullUrl: 'https://img.alicdn.com/imgextra/i2/2327995847/O1CN01SJyRxT1t3ylGVThru_!!2327995847.jpg'
+    },
+    {
+      id: 'default4',
+      thumbUrl: 'https://img.alicdn.com/imgextra/i2/2327995847/O1CN01qJZoVS1t3ylL5Tgh2_!!2327995847.jpg',
+      fullUrl: 'https://img.alicdn.com/imgextra/i2/2327995847/O1CN01qJZoVS1t3ylL5Tgh2_!!2327995847.jpg'
+    },
+    {
+      id: 'default5',
+      thumbUrl: 'https://img.alicdn.com/imgextra/i2/2327995847/O1CN01QPixHV1t3ylJvwJ6U_!!2327995847.jpg',
+      fullUrl: 'https://img.alicdn.com/imgextra/i2/2327995847/O1CN01QPixHV1t3ylJvwJ6U_!!2327995847.jpg'
+    },
+    {
+      id: 'default6',
+      thumbUrl: 'https://img.alicdn.com/imgextra/i1/2327995847/O1CN018Cac8M1t3ylHymUcQ_!!2327995847.jpg',
+      fullUrl: 'https://img.alicdn.com/imgextra/i1/2327995847/O1CN018Cac8M1t3ylHymUcQ_!!2327995847.jpg'
+    }
+  ]
+};
